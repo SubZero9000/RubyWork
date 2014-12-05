11626c69cc81c05b4cbbff365c178724971e35c3
SubZero9000 <kabir.roshan@hotmail.co.uk>
2014-12-05 10:57:16 +0000

    Final solution to pastabake.rb

--- pastabake.rb
+++ pastabake.rb
@@ -1,13 +1,3 @@
-#Inputpart
-puts "What recipe would you like to prepare?\n
-1. Pasta Bake
-2. Con Carne"
-choice_recipe = gets.chomp 
-puts "how many are going to be eating? "
-choice_seating = gets.chomp.to_i 	
-
-puts choice_recipe
-puts choice_seating
 #Pastabake.rb
 def food(dish,sitting)
 	require 'csv'
@@ -21,5 +11,16 @@ def food(dish,sitting)
 		end
 	end
 end
+
+#Inputpart
+puts "What recipe would you like to prepare?\n
+1. Pasta Bake
+2. Con Carne"
+choice_recipe = gets.chomp 
+puts "how many are going to be eating? "
+choice_seating = gets.chomp.to_i 	
+
+puts choice_recipe
+puts choice_seating
 
 food(choice_recipe, choice_seating)
